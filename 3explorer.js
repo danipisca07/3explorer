6017462b25fea56902b8e1ecceafda035f320535
danipisca07 <danielepiscaglia@libero.it>
2020-05-29 23:39:47 +0200

    Aggiunto OBJ loader

--- 3explorer.js
+++ 3explorer.js
@@ -1,84 +1,18 @@
 import * as THREE from 'https://unpkg.com/three@0.108.0/build/three.module.js';
-import {OrbitControls} from 'https://unpkg.com/three@0.108.0/examples/jsm/controls/OrbitControls.js';
-function main(){
-    const canvas = document.querySelector('#c');
-    const renderer = new THREE.WebGLRenderer({canvas});
+import {OBJLoader2} from 'https://unpkg.com/three@0.108.0/examples/jsm/loaders/OBJLoader2.js'
+import {MTLLoader} from 'https://unpkg.com/three@0.108.0/examples/jsm/loaders/MTLLoader.js';
+import {MtlObjBridge} from 'https://unpkg.com/three@0.108.0/examples/jsm/loaders/obj2/bridge/MtlObjBridge.js';
 
-    const fov = 75;
-    const aspect = 2;  // the canvas default
-    const near = 0.1;
-    const far = 5;
-    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    camera.position.z = 2;
-
-    const scene = new THREE.Scene();
-    scene.background = new THREE.Color(0xAAAAAA); //Colore background
-
-    //Luce direzionale
-    {
-        const color = 0xFFFFFF;
-        const intensity = 1;
-        const light = new THREE.DirectionalLight(color, intensity); //Default posizione e orientamento = (0,0,0)
-        light.position.set(-1, 2, 4); //Cambio la posizione
-        scene.add(light);
-    }
-
-    function makeInstance(geometry, color, posX){
-        const material = new THREE.MeshPhongMaterial({color});
-        const mesh = new THREE.Mesh(geometry, material);
-        scene.add(mesh);
-        mesh.position.x = posX;
-        return mesh;
-    }
-
-    //Cubi
-    let cubes;
-    {
-        const boxWidth = 1;
-        const boxHeight = 1;
-        const boxDepth = 1;
-        const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
-
-        cubes = [
-            makeInstance(geometry, 0x44aa88,  0),
-            makeInstance(geometry, 0x8844aa, -2),
-            makeInstance(geometry, 0xaa8844,  2),
-        ];
-    }
-
-    function render(time){
-        time *= 0.001;
-
-        //Ridimensionamento responsive del canvas
-        if (resizeRendererToDisplaySize(renderer)) {
-            //Se ho ridimensionato il canvas devo aggiornare l'aspect della camera
-            const canvas = renderer.domElement;
-            camera.aspect = canvas.clientWidth / canvas.clientHeight;
-            camera.updateProjectionMatrix();
-        }
-
-        cubes.forEach((cube, ndx) => { //Animazione con velocità differente per ogni cubo
-            const speed = 1 + ndx * .1;
-            const rot = time * speed;
-            cube.rotation.x = rot;
-            cube.rotation.y = rot;
+function loadObj(scene, path){
+    const objLoader = new OBJLoader2();
+    const mtlLoader = new MTLLoader();
+    mtlLoader.load(path+'.mtl', (mtlParseResult) => {
+        const materials =  MtlObjBridge.addMaterialsFromMtlLoader(mtlParseResult);
+        objLoader.addMaterials(materials);
+        objLoader.load(path+'.obj', (root) => {
+            scene.add(root);
         });
-
-        renderer.render(scene, camera);
-        requestAnimationFrame(render);
-    }
-    requestAnimationFrame(render);
-
-    //Aggiorna le dimensioni del rendering del canvas a quelle effettive del canvas sulla pagina
-    function resizeRendererToDisplaySize(renderer) {
-        const canvas = renderer.domElement;
-        const width = canvas.clientWidth;
-        const height = canvas.clientHeight;
-        const needResize = canvas.width !== width || canvas.height !== height;
-        if (needResize) {
-            renderer.setSize(width, height, false);
-        }
-        return needResize;
-    }
+    });
 }
-export { main };
+
+export {loadObj};
---
+++ demo.js
@@ -0,0 +1,62 @@
+import * as THREE from 'https://unpkg.com/three@0.108.0/build/three.module.js';
+import {OrbitControls} from 'https://unpkg.com/three@0.108.0/examples/jsm/controls/OrbitControls.js';
+import { loadObj } from "./3explorer.js";
+
+function main(){
+    const canvas = document.querySelector('#c');
+    const renderer = new THREE.WebGLRenderer({canvas});
+
+    const fov = 75;
+    const aspect = 2;  // the canvas default
+    const near = 0.1;
+    const far = 1000;
+    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+
+    const controls = new OrbitControls(camera, canvas);
+    controls.target.set(0, 50, 0);
+    controls.update();
+
+
+    const scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xAAAAAA); //Colore background
+
+    //Luce direzionale
+    {
+        const color = 0xFFFFFF;
+        const intensity = 1;
+        const light = new THREE.DirectionalLight(color, intensity); //Default posizione e orientamento = (0,0,0)
+        light.position.set(-1, 2, 4); //Cambio la posizione
+        scene.add(light);
+    }
+
+    loadObj(scene, 'assets/IronMan/IronMan');
+
+    function render(time){
+        time *= 0.001;
+
+        //Ridimensionamento responsive del canvas
+        if (resizeRendererToDisplaySize(renderer)) {
+            //Se ho ridimensionato il canvas devo aggiornare l'aspect della camera
+            const canvas = renderer.domElement;
+            camera.aspect = canvas.clientWidth / canvas.clientHeight;
+            camera.updateProjectionMatrix();
+        }
+
+        renderer.render(scene, camera);
+        requestAnimationFrame(render);
+    }
+    requestAnimationFrame(render);
+
+    //Aggiorna le dimensioni del rendering del canvas a quelle effettive del canvas sulla pagina
+    function resizeRendererToDisplaySize(renderer) {
+        const canvas = renderer.domElement;
+        const width = canvas.clientWidth;
+        const height = canvas.clientHeight;
+        const needResize = canvas.width !== width || canvas.height !== height;
+        if (needResize) {
+            renderer.setSize(width, height, false);
+        }
+        return needResize;
+    }
+}
+export { main };
