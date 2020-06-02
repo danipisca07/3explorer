ad1b33e5e6f46452d8c9e2b0b36ffec6e2a157e3
danipisca07 <danielepiscaglia@libero.it>
2020-06-02 23:34:05 +0200

    Fix jump e collisioni (verticali)

--- 3explorer.js
+++ 3explorer.js
@@ -11,6 +11,7 @@ import { DRACOLoader } from 'https://unpkg.com/three@0.108.0/examples/jsm/loader
 /*
 Model loaders
  */
+let modelRoot;
 function loadObj(scene, path){
     const objLoader = new OBJLoader2();
     const mtlLoader = new MTLLoader();
@@ -29,16 +30,16 @@ function loadGLTF(scene, path, camera, controls){
     dracoLoader.setDecoderPath( './js/libs/draco/' );
     gltfLoader.setDRACOLoader( dracoLoader );
     gltfLoader.load(path, (gltf) => {
-        const root = gltf.scene;
-        scene.add(root);
-        root.children.forEach((obj) => {
+        modelRoot = gltf.scene;
+        scene.add(modelRoot);
+        modelRoot.children.forEach((obj) => {
             if (obj.castShadow !== undefined) {
                 obj.castShadow = true;
                 obj.receiveShadow = true;
             }
         });
         if(camera != undefined){
-            const box = new THREE.Box3().setFromObject(root);
+            const box = new THREE.Box3().setFromObject(modelRoot);
             const boxSize = box.getSize(new THREE.Vector3()).length();
             const boxCenter = box.getCenter(new THREE.Vector3());
             console.log('boxSize: '+boxSize);
@@ -75,6 +76,20 @@ function frameArea(sizeToFitOnScreen, boxSize, boxCenter, camera) {
 CONTROLS
  */
 let controls;
+let moveForward = false;
+let moveBackward = false;
+let moveLeft = false;
+let moveRight = false;
+let canJump = false;
+let initialPos = [0,0,0];
+let heightFromGround = 1;
+let prevTime = performance.now();
+let speed = 200.0; //Velocità per movimento
+let jumpSpeed = 20.0;
+let mass = 10.0;
+let velocity = new THREE.Vector3();
+let direction = new THREE.Vector3();
+let raycaster = new THREE.Raycaster( new THREE.Vector3(), new THREE.Vector3( 0, - 1, 0 ), 0, heightFromGround ); //Cambia con height
 function enableControls (scene, camera, canvas){
     controls = new PointerLockControls( camera, canvas );
     canvas.addEventListener('mousedown', () =>{
@@ -91,143 +106,122 @@ function enableControls (scene, camera, canvas){
 
     controls.addEventListener( 'unlock', function () {
         //console.log("unlock");
+        moveForward = moveBackward = moveLeft = moveRight = false;
     } );
     scene.add(controls.getObject());
 }
 
-/*
-KEYBOARD EVENTS
- */
-let doStep;
-let initialPos = [0,0,0];
+
 function setInitialPosition(camera,x,y,z){
     camera.position.set(x,y,z);
     initialPos = [x,y,z];
 }
-{
-
-    let moveForward = false;
-    let moveBackward = false;
-    let moveLeft = false;
-    let moveRight = false;
-    let canJump = false;
-
-    function keyDownHandler (event){
-        switch ( event.keyCode ) {
-
-            case 38: // up
-            case 87: // w
-                moveForward = true;
-                break;
-
-            case 37: // left
-            case 65: // a
-                moveLeft = true;
-                break;
-
-            case 40: // down
-            case 83: // s
-                moveBackward = true;
-                break;
-
-            case 39: // right
-            case 68: // d
-                moveRight = true;
-                break;
-
-            case 32: // space
-                if ( canJump === true ) velocity.y += 350;
-                canJump = false;
-                break;
-
-        }
-    }
-    function keyUpHandler( event ) {
-
-        switch ( event.keyCode ) {
-
-            case 38: // up
-            case 87: // w
-                moveForward = false;
-                break;
-
-            case 37: // left
-            case 65: // a
-                moveLeft = false;
-                break;
-
-            case 40: // down
-            case 83: // s
-                moveBackward = false;
-                break;
-
-            case 39: // right
-            case 68: // d
-                moveRight = false;
-                break;
-
-        }
-
-    }
-    document.addEventListener( 'keydown', keyDownHandler, false );
-    document.addEventListener( 'keyup', keyUpHandler, false );
-
-    let prevTime = performance.now();
-    let velocity = new THREE.Vector3();
-    let direction = new THREE.Vector3();
-    let vertex = new THREE.Vector3();
-    let color = new THREE.Color();
-    let raycaster = new THREE.Raycaster( new THREE.Vector3(), new THREE.Vector3( 0, - 1, 0 ), 0, 10 );
-    doStep = function (scene){
-        if ( controls.isLocked === true ) {
-
-            raycaster.ray.origin.copy( controls.getObject().position );
-            raycaster.ray.origin.y -= 10;
-
-            var intersections = raycaster.intersectObjects( scene.children );
-
-            var onObject = intersections.length > 0;
-
-            var time = performance.now();
-            var delta = ( time - prevTime ) / 1000;
-
-            velocity.x -= velocity.x * 10.0 * delta;
-            velocity.z -= velocity.z * 10.0 * delta;
-
-            velocity.y -= 9.8 * 100.0 * delta; // 100.0 = mass
-
-            direction.z = Number( moveForward ) - Number( moveBackward );
-            direction.x = Number( moveRight ) - Number( moveLeft );
-            direction.normalize(); // this ensures consistent movements in all directions
-
-            if ( moveForward || moveBackward ) velocity.z -= direction.z * 400.0 * delta;
-            if ( moveLeft || moveRight ) velocity.x -= direction.x * 400.0 * delta;
-
-            if ( onObject === true ) {
-
-                velocity.y = Math.max( 0, velocity.y );
-                canJump = true;
-
-            }
-
-            controls.moveRight( - velocity.x * delta );
-            controls.moveForward( - velocity.z * delta );
-
-            controls.getObject().position.y += ( velocity.y * delta ); // new behavior
-
-            if ( controls.getObject().position.y < initialPos[1] ) {
-
-                velocity.y = 0;
-                controls.getObject().position.y = initialPos[1];
-
-                canJump = true;
-
-            }
-
-            prevTime = time;
-
-        }
-    };
+function setHeight(height){
+    heightFromGround = height;
 }
+function keyDownHandler (event){
+    switch ( event.keyCode ) {
+
+        case 38: // up
+        case 87: // w
+            moveForward = true;
+            break;
+
+        case 37: // left
+        case 65: // a
+            moveLeft = true;
+            break;
+
+        case 40: // down
+        case 83: // s
+            moveBackward = true;
+            break;
+
+        case 39: // right
+        case 68: // d
+            moveRight = true;
+            break;
+
+        case 32: // space
+            if ( canJump === true ) velocity.y += jumpSpeed;
+            canJump = false;
+            break;
+
+    }
+}
+function keyUpHandler( event ) {
+
+    switch ( event.keyCode ) {
+
+        case 38: // up
+        case 87: // w
+            moveForward = false;
+            break;
+
+        case 37: // left
+        case 65: // a
+            moveLeft = false;
+            break;
+
+        case 40: // down
+        case 83: // s
+            moveBackward = false;
+            break;
+
+        case 39: // right
+        case 68: // d
+            moveRight = false;
+            break;
+
+    }
+
+}
+document.addEventListener( 'keydown', keyDownHandler, false );
+document.addEventListener( 'keyup', keyUpHandler, false );
+
+
+function doStep(scene){
+    let groundPoint;
+    var time = performance.now();
+    var delta = ( time - prevTime ) / 1000;
+
+    velocity.x -= velocity.x * mass * delta;
+    velocity.z -= velocity.z * mass * delta;
+
+    velocity.y -= 9.8 * mass * delta; // 100.0 = mass
+
+    direction.z = Number( moveForward ) - Number( moveBackward );
+    direction.x = Number( moveRight ) - Number( moveLeft );
+    direction.normalize(); // this ensures consistent movements in all directions
+
+    if ( moveForward || moveBackward ) velocity.z -= direction.z * speed * delta;
+    if ( moveLeft || moveRight ) velocity.x -= direction.x * speed * delta;
+
+    controls.moveRight( - velocity.x * delta );
+    controls.moveForward( - velocity.z * delta );
+
+    let intersections;
+    if(modelRoot != undefined){
+        raycaster.ray.origin.copy( controls.getObject().position )
+        intersections = raycaster.intersectObject(scene, true ); //TODO: ottimizza con un ciclo ricorsivo su ogni oggetto della scena e ti fermi al
+    }
+
+    controls.getObject().position.y += ( velocity.y * delta ); // new behavior
+
+    if(intersections !== undefined && intersections.length > 0 && controls.getObject().position.y - heightFromGround<intersections[0].point.y)
+    {
+        console.log(intersections.length);
+        velocity.y = Math.max( 0, velocity.y );
+        controls.getObject().position.y = intersections[0].point.y + heightFromGround;
+        canJump = true;
+    }else if(controls.getObject().position.y < initialPos[1] ) {
+        velocity.y = 0;
+        controls.getObject().position.y = initialPos[1];
+        canJump = true;
+    }
+
+
+    prevTime = time;
+};
 
 export {loadObj, loadGLTF, enableControls, doStep, setInitialPosition};
